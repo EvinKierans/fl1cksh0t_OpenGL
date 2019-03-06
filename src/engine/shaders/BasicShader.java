ab6a71df79ed111fe925a39476e62c963ed9857e
Real-EviN <evin.kierans100@gmail.com>
2019-03-06 18:08:33 +0000

    Added shaders and rendering

---
+++ src/engine/shaders/BasicShader.java
@@ -0,0 +1,16 @@
+package engine.shaders;
+
+public class BasicShader extends Shader {
+
+    private static final String VERTEX_FILE = "src/engine/shaders/basicVertexShader.vs",
+                                FRAGMENT_FILE = "src/engine/shaders/basicFragmentShader.fs";
+
+    public BasicShader() {
+        super(VERTEX_FILE, FRAGMENT_FILE);
+
+    }
+
+    public void bindAllAttributes() {
+        super.bindAttribute(0, "vertices");
+    }
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -3,15 +3,19 @@ package main;
 import engine.io.Window;
 import engine.render.Model;
 import engine.render.Renderer;
+import engine.shaders.BasicShader;
 
 public class Main {
     public static final int WIDTH = 800, HEIGHT = 600, FPS = 60;
     public static Window window = new Window(WIDTH, HEIGHT, FPS, "fl1cksh0t OpenGL");
     public static Renderer renderer = new Renderer();
+    public static BasicShader shader = new BasicShader();
 
     public static void main(String args[]){
         window.create();
         window.setBackgroundColor(1.0f, 0.0f, 0.0f);
+
+        shader.create();
 
         Model model = new Model(new float[] {
                 -0.5f, 0.5f, 0.0f,  //TOP LEFT      0
@@ -27,13 +31,15 @@ public class Main {
         while(!window.closed()) {
             if(window.isUpdating()) {
                 window.update();
+                shader.bind();
                 renderer.renderModel(model);
                 window.swapBuffers();
 
             }
         }
 
-        window.stop();
+        shader.remove();
         model.remove();
+        window.stop();
     }
 }
